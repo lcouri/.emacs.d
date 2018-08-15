6ac3f575ab3396b674a92716846dc7de99d1aaf1
lcouri <l.g.couri@googlemail.com>
2018-08-15 22:50:55 +0100

    Work around for VcXsrv not supporting double buffering.

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
+;; I'm also running emacs on WSL (Windows Subsystem for Linux) with VcXsrv as an X server.
+;; It doesn't seem to support double buffering which causes emacs to not resize its frame.
+;; I'm not sure if there's a better way of checking in emacs whether the x server supports
+;; double buffering, but for now at least I'm just checking a custom environment variable.
+(when (getenv "NO_X_DOUBLE_BUFFERING")
+  (setq default-frame-alist
+      (append default-frame-alist '((inhibit-double-buffering . t)))))
+
 ;; Disable UI stuff immediately to avoid any display at all
 (when window-system
   (tool-bar-mode -1)
