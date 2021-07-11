361b3e81ea6a2002192979aa25e62150a5267cfc
lcouri <l.g.couri@googlemail.com>
2021-07-11 11:47:04 +0200

    Update org package-archives

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
 ;; Set up package archives
 (with-eval-after-load 'package
   (add-to-list 'package-archives
-               '("melpa" . "https://melpa.org/packages/") t)
+	       '("melpa" . "https://melpa.org/packages/") t)
   (add-to-list 'package-archives
 	       '("melpa-stable" . "https://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives
-               '("org" . "https://orgmode.org/elpa/") t))
+	       '("nongnu" . "https://elpa.nongnu.org/nongnu/") t))
 
 ;; Install use-package if it's not already installed.
 (unless (package-installed-p 'use-package)
